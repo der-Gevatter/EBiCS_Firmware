fefb1ed77cb38737ce213429bf72dbeb9c520a85
stephan <stephan@Stephan-PC.fritz.box>
2024-03-18 21:07:03 +0100

    init.h

---
+++ Inc/init.h
@@ -0,0 +1,18 @@
+/*
+ * init.h
+ *
+ *  Created on: 18.03.2024
+ *      Author: stephan
+ */
+
+#ifndef INIT_H_
+#define INIT_H_
+
+//#include "stm32f1xx_hal.h"
+
+/* Private function prototypes -----------------------------------------------*/
+
+
+/* END Private function prototypes -----------------------------------------------*/
+
+#endif /* INIT_H_ */
