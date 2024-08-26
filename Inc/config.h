af1393fced3fb0df5a75d32dc7a644e026d77ec2
der-Gevatter <145274282+der-Gevatter@users.noreply.github.com>
2024-08-26 16:05:53 +0200

    DISPLAY_TYPE_KINGMETER_901U

--- Inc/config.h
+++ Inc/config.h
@@ -98,7 +98,7 @@
 //---------------------------------------------------------------------
 //Display settings
 //#define DISPLAY_TYPE DISPLAY_TYPE_DEBUG
-#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_618U
+#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_901U
 
 //---------------------------------------------------------------------
 //Regen settings
